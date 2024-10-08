e5248ab4d5a0ce55ea4786f6271fa156c51eb105
Miguel Abril <miguel.abril.alamo.al@iespoligonosur.org>
2024-10-08 12:03:39 +0200

    creo prestamo

--- ejemplo/admin.py
+++ ejemplo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Cliente, Biblioteca, datosCliente, Autor, Libros
+from .models import Cliente, Biblioteca, datosCliente, Autor, Libros, Prestamos
 
 # Register your models here.
 admin.site.register(Cliente)
@@ -7,4 +7,5 @@ admin.site.register(Biblioteca)
 admin.site.register(datosCliente)
 admin.site.register(Autor)
 admin.site.register(Libros)
+admin.site.register(Prestamos)
 
