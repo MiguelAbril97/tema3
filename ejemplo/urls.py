fdbc65fa3ab08e4193ceae72c02bfe02cdcaa450
Miguel Abril <miguel.abril.alamo.al@iespoligonosur.org>
2024-10-18 14:38:36 +0200

    añado mas url

--- ejemplo/urls.py
+++ ejemplo/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
@@ -7,5 +7,7 @@ urlpatterns = [
     path('libros/<int:id_libros>/', views.dame_libro, name='dame_libro'),
     path('libros/listar/<int:anyo_libros>/<int:mes_libros>',views.dame_libros_fecha, name="dame_libros_fecha"),
     path('libros/listar/<str:idioma>/', views.dame_libros_idioma,name="dame_libros_idioma"),
-    path('biblioteca/<int: id_biblioteca>/libros/<str:descripcion_libros>',views.dame_libros_biblioteca, name="dame_libros_biblioteca"),
+    path('biblioteca/<int:id_biblioteca>/libros/<str:descripcion_libros>',views.dame_libros_biblioteca, name="dame_libros_biblioteca"),
+    path('ultimo-cliente-libro/<int:libro>',views.dame_ultimo_cliente_libro,name='ultimo-cliente-libro'),
+    re_path(r"^filtro[0-9]$", views.libros_no_prestados, name="libros_no_prestados"),
 ]
--- ejemplo/views.py
+++ ejemplo/views.py
@@ -29,4 +29,12 @@ def dame_libros_idioma(request, idioma):
 def dame_libros_biblioteca (request, id_biblioteca, descripcion_libros):
     libros=Libros.objects.select_related('biblioteca').prefetch_related('autores')
     libros = libros.filter(biblioteca = id_biblioteca).filter(descripcion__contains=descripcion_libros)
-    
+
+def dame_ultimo_cliente_libro(request, libro):
+    cliente = Cliente.objects.filter(prestamos__libro=libro).order_by('-prestamos__fecha_prestamo')[:1].get()
+    return render (request,'cliente/cliente.html',{'cliente':cliente})
+
+def libros_no_prestados(request):
+    libros=Libros.objects.select_related("biblioteca").prefetch_related("autores")
+    libros = libros.filter(prestamos=None)
+    return render(request, 'libro/lista.html',{'libros_mostrar':libros})
